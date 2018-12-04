224be1a3278f6d817de7b81112e0d2da21846289
Eirik Kvarstein <eirik.kvarstein@marcello.no>
2018-12-04 10:20:37 +0100

    Day 4 cleanup

--- 04/main.go
+++ 04/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"adventOfCode2018/common"
-	"fmt"
 	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 type guard struct {
@@ -21,20 +22,15 @@ func getMin(line string) (int, error) {
 	return strconv.Atoi(m[1])
 }
 
-func main() {
-	lines := common.ReadLines("./input.txt")
-	sort.Strings(lines)
+func parseGuards(lines []string) map[string]*guard {
 	guards := make(map[string]*guard)
 	gid := ""
-	wakeIndex := 0
 	sleepIndex := 0
 	for i, l := range lines {
 		s := strings.Split(l, " ")
 		status := s[2]
 		if strings.Contains(status, "Guard") {
 			gid = s[3]
-			wakeIndex = -1
-			sleepIndex = -1
 			if _, ok := guards[gid]; !ok {
 				guards[gid] = &guard{
 					id:    gid,
@@ -43,72 +39,74 @@ func main() {
 			}
 		}
 		if strings.Contains(status, "wakes") {
-			wakeIndex = i
 			from, _ := getMin(lines[sleepIndex])
-			to, _ := getMin(lines[wakeIndex])
+			to, _ := getMin(lines[i])
 			guards[gid].slept += to - from
 			for j := from; j < to; j++ {
 				guards[gid].minutes = append(guards[gid].minutes, j)
 			}
-			// fmt.Printf("guard %v wakes up (%v - %v)\n", gid, to, from)
-
 		}
 		if strings.Contains(status, "falls") {
 			sleepIndex = i
 		}
-
 	}
+	return guards
+}
 
+func main() {
+	lines := common.ReadLines("./input.txt")
+	sort.Strings(lines)
+	guards := parseGuards(lines)
 	max := 0
 	var sleeper *guard
 	for _, g := range guards {
-		// log.Printf("%v slept %v min", k, v.slept)
+		logrus.Debugf("%v slept %v min", g.id, g.slept)
 		if g.slept > max {
 			max = g.slept
 			sleeper = g
 		}
 	}
-	fmt.Printf("sleeper: %v: %v", sleeper.id, sleeper.slept)
-	ms := make(map[int]int)
+	logrus.Infof("Most sleepy guard: %v (%v minutes)", sleeper.id, sleeper.slept)
+	mm := make(map[int]int)
 	for _, v := range sleeper.minutes {
-		ms[v]++
+		mm[v]++
 	}
 
 	max = 0
 	minute := 0
-	for k, v := range ms {
+	for k, v := range mm {
 		if v > max {
 			max = v
 			minute = k
 		}
 	}
-	fmt.Printf("minute most slept: %v: %v\n", minute, max) // 87681
+	id, _ := strconv.Atoi(strings.Split(sleeper.id, "#")[1])
+	logrus.Infof("Minute most slept: %v (%v times)", minute, max)
+	logrus.Infof("Part 1: %v", id*minute) // 87681
 
 	// part 2
 	type gs struct {
-		guard   *guard
 		minutes map[int]int
 	}
-	ms2 := make(map[*guard]*gs)
+	sleepyGuards := make(map[*guard]*gs)
 	max = 0
 	minute = 0
 	var gg guard
 	for _, g := range guards {
 		for _, v := range g.minutes {
-			if _, ok := ms2[g]; !ok {
-				ms2[g] = &gs{
+			if _, ok := sleepyGuards[g]; !ok {
+				sleepyGuards[g] = &gs{
 					minutes: make(map[int]int),
 				}
 			}
-			ms2[g].minutes[v]++
-			if ms2[g].minutes[v] > max {
-				max = ms2[g].minutes[v]
+			sleepyGuards[g].minutes[v]++
+			if sleepyGuards[g].minutes[v] > max {
+				max = sleepyGuards[g].minutes[v]
 				minute = v
 				gg = *g
 			}
 		}
 	}
-	id, _ := strconv.Atoi(strings.Split(gg.id, "#")[1])
-	fmt.Printf("part 2: %v\n", id*minute)
-
+	id, _ = strconv.Atoi(strings.Split(gg.id, "#")[1])
+	logrus.Infof("Part 2: %v", id*minute)
 }
