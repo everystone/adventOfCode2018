342262ddd38bc188f162c5a173e5a774fc246d5c
Eirik Kvarstein <eirik.kvarstein@gmail.com>
2018-12-05 07:55:14 +0100

    Day 5 completed

--- 05/main.go
+++ 05/main.go
@@ -8,19 +8,21 @@ import (
 )
 
 // dabAcCaCBAcCcaDA
-func process(str string, m string, custom bool) (bool, string) {
+func process(str string, remove string) (bool, string) {
+	if remove != "" {
+		str = strings.Replace(str, strings.ToLower(remove), "", -1)
+		str = strings.Replace(str, strings.ToUpper(remove), "", -1)
+	}
 	l := len(str)
 	for i, c := range str {
 		if i == l-1 {
 			return false, str
 		}
 		s := string(c)
-		if !custom {
-			m = s
-		}
-		// logrus.Infof("checking %v & %v", strings.ToLower(m), strings.ToUpper(m))
-		if s == strings.ToLower(m) && string(str[i+1]) == strings.ToUpper(m) ||
-			s == strings.ToUpper(m) && string(str[i+1]) == strings.ToLower(m) {
+
+		// logrus.Infof("checking %v & %v", strings.ToLower(s), strings.ToUpper(s))
+		if s == strings.ToLower(s) && string(str[i+1]) == strings.ToUpper(s) ||
+			s == strings.ToUpper(s) && string(str[i+1]) == strings.ToLower(s) {
 			// remove current + next
 			match := str[i : i+2]
 			//logrus.Infof("match: %v", match)
@@ -37,24 +39,22 @@ func main() {
 	raw := common.ReadLines("./input.txt")[0]
 	input := raw
 
-	// logrus.Infof("len of str %v", len(input))
 	running := true
-	// for running == true {
-	// 	running, input = process(input, "", false)
-	// }
-	// //logrus.Infof("%v", input)
-	// logrus.Infof("len of str %v", len(input)) // 9296
+	for running == true {
+		running, input = process(input, "")
+	}
 
-	letters := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	logrus.Infof("Part 1: %v", len(input)) // 9296
+
+	letters := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "s", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 	min := len(raw)
 	best := ""
 	for _, l := range letters {
 
 		input = raw
 		running = true
-		// logrus.Infof("testing unit %v %v", l, len(input))
 		for running == true {
-			running, input = process(input, l, true)
+			running, input = process(input, l)
 		}
 		result := len(input)
 		if result < min {
@@ -64,5 +64,5 @@ func main() {
 		logrus.Infof("result of unit %v: %v", l, result)
 
 	}
-	logrus.Infof("winner: %v (%v)", best, min)
+	logrus.Infof("Part 2: %v (%v)", min, best) // 5534, o
 }
