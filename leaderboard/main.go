9857cff3fb9025917fb43a9e39364d3f1417d0af
Eirik Kvarstein <eirik.kvarstein@marcello.no>
2018-12-05 10:04:24 +0100

    Formatted leaderboard output & added date.

--- leaderboard/main.go
+++ leaderboard/main.go
@@ -9,22 +9,22 @@ import (
 	"time"
 )
 
-type Star struct {
+type star struct {
 	Timestamp string `json:"get_star_ts"`
 }
 
-type Member struct {
+type member struct {
 	LocalScore int `json:"local_score"`
 	//LastStarTs  string `json:"last_star_ts"`
 	Stars       int                  `json:"stars"`
 	GlobalScore int                  `json:"global_score"`
 	ID          string               `json:"id"`
 	Name        string               `json:"name"`
-	Completion  map[int]map[int]Star `json:"completion_day_level"`
+	Completion  map[int]map[int]star `json:"completion_day_level"`
 }
 
-type Members struct {
-	Members map[string]Member `json:"members"`
+type members struct {
+	Members map[string]member `json:"members"`
 }
 
 func main() {
@@ -36,7 +36,7 @@ func main() {
 	}
 
 	//fmt.Printf("%v", byteValue)
-	var m Members
+	var m members
 	err = json.Unmarshal(byteValue, &m)
 	if err != nil {
 		panic(err)
@@ -55,7 +55,7 @@ func main() {
 				i, _ = strconv.ParseInt(v[2].Timestamp, 10, 64)
 				ts2 := time.Unix(i, 0)
 				result := ts2.Sub(ts1).Minutes()
-				fmt.Printf("%v day %v p2: %.2f minutes\n", mem.Name, day, result)
+				fmt.Printf("%v day %v\t%.2f minutes\t(%v)\n", mem.Name, day, result, ts2.Format("15:04:05"))
 				times[mem.Name] = append(times[mem.Name], result)
 			}
 		}
@@ -65,7 +65,8 @@ func main() {
 			for _, i := range times[mem.Name] {
 				sum += i
 			}
-			fmt.Printf("%v avg part2 time: %.2f minutes\n", mem.Name, sum/float64(len(times[mem.Name])))
+			fmt.Printf("%v avg:  \t%.2f minutes\n", mem.Name, sum/float64(len(times[mem.Name])))
+			fmt.Printf("-----------------------------------------------\n")
 		}
 	}
 	// fmt.Printf("%v", m.Members["372116"])
