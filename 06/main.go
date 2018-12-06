534d99ec132a30addbfc4aad64e865c161d84a9f
Eirik Kvarstein <eirik.kvarstein@marcello.no>
2018-12-06 10:49:31 +0100

    Day 6 part 1

--- 06/main.go
+++ 06/main.go
@@ -3,27 +3,28 @@ package main
 import (
 	"adventOfCode2018/common"
 	"fmt"
+	"log"
 	"math"
 )
 
-type coord struct {
-	x    int
-	y    int
-	inf  bool
-	size int
+type coordinate struct {
+	x       int
+	y       int
+	size    int
+	trapped bool
 }
 
-func (p *coord) String() string {
-	return "A"
+func (p *coordinate) String() string {
+	return fmt.Sprintf("%v,%v", p.x, p.y)
 }
 
-func getDistance(p1 *coord, p2 *coord) int {
+func manhattan(p1 coordinate, p2 coordinate) int {
 	res := math.Abs(float64(p2.x-p1.x)) + math.Abs(float64(p2.y-p1.y))
 	return int(res)
 }
 
-var coordinates []*coord
-var grid [300][300]*coord
+var coordinates []*coordinate
+var grid [400][400]*coordinate
 
 func main() {
 
@@ -31,25 +32,72 @@ func main() {
 	for _, line := range lines {
 		var x, y int
 		fmt.Sscanf(line, "%d, %d", &x, &y)
-		p := coord{x, y, false, 0}
+		p := coordinate{x, y, 0, false}
 		grid[p.x][p.y] = &p
 		coordinates = append(coordinates, &p)
 	}
 	fmt.Printf("num coordinates: %v\n", len(coordinates))
+	for x := 0; x < 400; x++ {
+		for y := 0; y < 400; y++ {
+			pos := coordinate{x, y, 0, false}
 
-	// loop grid
-	for i := 0; i < 300; i++ {
-		for j := 0; j < 300; j++ {
-			// find coord closest to i,j
-			dist := getDistance(p1, p2)
-			fmt.Printf("distance: %v\n", dist)
+			min := 400
+			var c *coordinate
+			for _, coordinate := range coordinates {
+				dist := manhattan(*coordinate, pos)
+				if dist < min {
+					c = coordinate
+					min = dist
+				}
+			}
+			grid[x][y] = c
+			c.size++
+			// fmt.Printf("%v belongs to %v\n", pos, c)
 		}
 	}
-	num := 0
-	for _, p := range coordinates {
-		if p.inf {
-			num++
+	// calc trapped
+	for _, c1 := range coordinates {
+		left, right, top, bot := false, false, false, false
+		// check horizontal
+		for x := 0; x < 400; x++ {
+			c2 := grid[x][c1.y]
+			if c1 == c2 {
+				continue
+			}
+			if x < c1.x {
+				left = true
+			}
+			if x > c1.x {
+				right = true
+			}
+		}
+		// check vertical
+		for y := 0; y < 400; y++ {
+			c2 := grid[c1.x][y]
+			if c1 == c2 {
+				continue
+			}
+			if y > c1.y {
+				top = true
+			}
+			if y < c1.y {
+				bot = true
+			}
+		}
+		c1.trapped = left && right && bot && top
+	}
+
+	max, numtrapped := 0, 0
+	var largest *coordinate
+	for _, c := range coordinates {
+		if c.trapped {
+			numtrapped++
+			if c.size > max {
+				max = c.size
+				largest = c
+			}
 		}
 	}
-	fmt.Printf("inf count: %v\n", num)
+	log.Printf("num trapped : %v\n", numtrapped)
+	log.Printf("largest trapped: %v: %v", largest, max) // not 10250
 }
